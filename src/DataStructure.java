e8b23768c821d96b4460a70499a522d10da199b5
Joe Wang <zhouqiang.wang@hotmail.com>
2019-09-13 09:39:11 -0700

    add DataStructure portal.

---
+++ src/DataStructure.java
@@ -0,0 +1,40 @@
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.math.BigInteger;
+import java.util.*;
+
+public class DataStructure {
+    private static final String FILE_INPUT = System.getProperty("user.dir") + "/src/input.txt";
+    private static final String FILE_OUTPUT = "output.txt";
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(new FileReader(FILE_INPUT));
+//        Scanner sc = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
+//        PrintWriter pw = new PrintWriter(new FileWriter(FILE_OUTPUT));
+        PrintWriter pw = new PrintWriter(System.out);
+
+        Map<Character, Integer> map = new HashMap<>();
+
+        SortedSet<Map.Entry<Character, Integer>> sortedSet = new TreeSet<>((o1, o2) -> o2.getValue() - o1.getValue());
+        sortedSet.addAll(map.entrySet());
+
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(1);
+        pw.println(queue);
+        queue.size();
+        queue.poll();
+
+        int[] arr = new int[4];
+        Arrays.sort(arr);
+
+        List<Integer> list = new ArrayList<>();
+        List<List<Integer>> listMap = new ArrayList<>(4);
+        pw.println(listMap);
+
+        pw.flush();
+        pw.close();
+        sc.close();
+    }
+}
+

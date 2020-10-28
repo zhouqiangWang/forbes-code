39d2239a780c602ffaadf08f08655c300851e8b6
Joe Wang <zhouqiang.wang@hotmail.com>
2020-10-28 00:16:02 -0700

    Merge BitManipulation.java

--- src/main/java/_BitManipulation.java
+++ src/main/java/_BitManipulation.java
@@ -28,6 +28,16 @@ public class _BitManipulation {
         return (n>0 && ((n&(n-1)) == 0));
     }
 
+
+    public static int reverseBits(int n) {
+        n = ((n >> 16) & 0x0000ffff) | (n << 16);
+        n = (((n & 0xff00ff00) >> 8) & 0x00ff00ff) | ((n & 0x00ff00ff) << 8);
+        n = (((n & 0xf0f0f0f0) >> 4) & 0x0f0f0f0f) | ((n & 0x0f0f0f0f) << 4);
+        n = (((n & 0xcccccccc) >> 2) & 0x33333333) | ((n & 0x33333333) << 2);
+        n = (((n & 0xaaaaaaaa) >> 1) & 0x55555555) | ((n & 0x55555555) << 1);
+        return n;
+    }
+
     public static void main(String[] args) {
         PrintWriter pw = new PrintWriter(System.out);
 
@@ -41,6 +51,14 @@ public class _BitManipulation {
         pw.println(isPower2(8));
 
         pw.println(lowBit(6));
+
+        n = 43261596;
+        pw.println(Integer.toBinaryString(n));
+        pw.println(Integer.toBinaryString(reverseBits(n)));
+
+        pw.println(Integer.toBinaryString(Integer.MIN_VALUE >> 2));
+        pw.println(Integer.toBinaryString(Integer.MIN_VALUE >>> 2));
+        pw.println(Integer.toBinaryString(Integer.MAX_VALUE >> 2));
 
         pw.flush();
         pw.close();
